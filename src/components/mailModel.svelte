417fa441235aab36a373de2bc273a18c85d30a99
jeremy saveries <jeremysaveries2@gmail.com>
2024-12-17 11:13:34 +0100

    creation du bouton pour renvoyer lutilisateur vers sa boite mail avec mon adresse préenregistrer mais css a revoir

---
+++ src/components/mailModel.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let email = "saveriesj@gmail.com"; // Adresse e-mail par défaut
+  export let buttonText = "Envoyer un email"; // Texte par défaut du bouton
+  export let buttonClass =
+    "bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition"; // Classes CSS
+</script>
+
+<button
+  class={buttonClass}
+  on:click={() =>
+    (window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`)}
+>
+  {buttonText}
+</button>
