d8964a1c79f3688ebd738ac813fb72f9703473d0
jeremy saveries <jeremysaveries2@gmail.com>
2025-02-15 18:36:46 +0100

    Updates

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-
-export default defineConfig({
-  base: "/jeremysaveries/portfolio-/", // Remplace bien par le nom de ton repo
-  plugins: [svelte()],
-});
