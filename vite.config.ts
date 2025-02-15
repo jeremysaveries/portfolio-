1d69d795a9e72df500031fc363f27a8ac746d2e4
jeremy saveries <jeremysaveries2@gmail.com>
2025-02-15 22:53:20 +0100

    modif de vite config pour dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   base: "/jeremysaveries/portfolio-/", // Remplace bien par le nom de ton repo
   plugins: [svelte()],
+  build: {
+    outDir: 'dist'  // Assure-toi que les fichiers de build vont dans ce dossier
+  }
 });
